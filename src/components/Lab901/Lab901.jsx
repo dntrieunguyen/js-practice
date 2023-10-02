f52bd0c7ce760b07082b738c4daa3dba059b220e
dntrieunguyen <dntrieunguyen@gmail.com>
2023-10-02 23:59:13 +0700

    update

--- src/components/Lab901/Lab901.jsx
+++ src/components/Lab901/Lab901.jsx
@@ -7,24 +7,24 @@ import { ContextProvider } from './store/Context';
 import { useReducer } from 'react';
 
 const data = [
-   // {
-   //    id: 'm1',
-   //    name: 'Sushi',
-   //    description: 'Finest fish and veggies',
-   //    price: 22.99,
-   // },
+   {
+      id: 'm1',
+      name: 'Sushi',
+      description: 'Finest fish and veggies',
+      price: 22.99,
+   },
    {
       id: 'm2',
       name: 'Schnitzel',
       description: 'A german specialty!',
       price: 16.5,
    },
-   // {
-   //    id: 'm3',
-   //    name: 'Barbecue Burger',
-   //    description: 'American, raw, meaty',
-   //    price: 12.99,
-   // },
+   {
+      id: 'm3',
+      name: 'Barbecue Burger',
+      description: 'American, raw, meaty',
+      price: 12.99,
+   },
    {
       id: 'm4',
       name: 'Green Bowl',
