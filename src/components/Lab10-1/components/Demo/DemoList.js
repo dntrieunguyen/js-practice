6c1a2a66386c1e583aa91300259612df42de9de1
dntrieunguyen <dntrieunguyen@gmail.com>
2023-10-13 09:16:41 +0700

    Complete Lab 10.1

--- src/components/Lab10-1/components/Demo/DemoList.js
+++ src/components/Lab10-1/components/Demo/DemoList.js
@@ -1,21 +1,23 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import classes from './DemoList.module.css';
 
 const DemoList = props => {
    const { items } = props;
 
-   console.log('Items sorted');
-   // const sortedList = items.sort((a, b) => a - b);
+   const sortedList = useMemo(() => {
+      console.log('Items sorted');
+      return items.sort((a, b) => a - b);
+   }, []);
 
    console.log('DemoList RUNNING');
    return (
       <div className={classes.list}>
          <h2>{props.title}</h2>
          <ul>
-            {/* {sortedList.map((item) => (
-          <li key={item}>{item}</li>
-        ))} */}
+            {sortedList.map(item => (
+               <li key={item}>{item}</li>
+            ))}
          </ul>
       </div>
    );
--- src/components/Lab10-1/components/UI/Button/Button.js
+++ src/components/Lab10-1/components/UI/Button/Button.js
@@ -1,19 +1,17 @@
 import React from 'react';
-
 import classes from './Button.module.css';
 
-const Button = (props) => {
-  console.log('Button RUNNING');
-  return (
-    <button
-      type={props.type || 'button'}
-      className={`${classes.button} ${props.className}`}
-      onClick={props.onClick}
-      disabled={props.disabled}
-    >
-      {props.children}
-    </button>
-  );
+const Button = props => {
+   return (
+      <button
+         type={props.type || 'button'}
+         className={`${classes.button} ${props.className}`}
+         onClick={props.onClickBtn}
+         disabled={props.disabled}
+      >
+         {props.children}
+      </button>
+   );
 };
 
 export default React.memo(Button);
