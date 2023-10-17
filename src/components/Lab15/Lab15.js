9421e074f5e03c2a1e5e4c74ec6995e639f9770a
dntrieunguyen <dntrieunguyen@gmail.com>
2023-10-17 17:14:06 +0700

    update

--- src/components/Lab15/Lab15.js
+++ src/components/Lab15/Lab15.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Counter from './Counter';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 export default function Lab15() {
+   const store = createStore();
    return (
       <>
-         <Counter></Counter>
+         <Provider store={store}>
+            <Counter></Counter>
+         </Provider>
       </>
    );
 }
