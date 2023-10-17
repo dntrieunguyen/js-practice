ebf71d47db1ed67686bc9761a80c5f00d77b1af6
dntrieunguyen <dntrieunguyen@gmail.com>
2023-10-17 16:54:28 +0700

    Lab 15

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Lab201 from './components/Lab201';
 import Lab701 from './components/Lab701/Lab701';
 import Lab901 from './components/Lab901/Lab901';
 import Lab1001 from './components/Lab10-1/components/Lab1001';
+import Lab15 from './components/Lab15/Lab15';
 function App() {
    return (
       <BrowserRouter>
@@ -14,6 +15,7 @@ function App() {
             <Route path="/lab701" element={<Lab701 />} />
             <Route path="/lab901" element={<Lab901 />} />
             <Route path="/lab1001" element={<Lab1001 />} />
+            <Route path="/lab15" element={<Lab15 />} />
          </Routes>
       </BrowserRouter>
    );
